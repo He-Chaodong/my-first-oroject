c194885594d32e7ce8a8106537e77f5b0e1c9d66
He-Chaodong <3859691461@qq.com>
2025-09-27 18:25:04 +0800

    Initial commit from Eclipse

---
+++ helloworld1.java
@@ -0,0 +1,11 @@
+package com.jay.helloworld1;
+
+public class helloworld1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World|你好世界！");
+		// TODO Auto-generated method stub
+
+	}
+
+}
